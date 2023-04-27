1e67e968d3b7b1471fae1b8fa70601d05a8635ff
stepan103ua <stepan8877ua@gmail.com>
2023-04-27 21:11:31 +0300

    Add code editor to navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/Auth/Login/LoginPage';
 import RegisterPage from './pages/Auth/Register/RegisterPage';
 import HomePage from './pages/Authorized/Home/HomePage';
-import { homeRoute, loginRoute, registerRoute } from './values/routes';
+import { codeEditorRoute, homeRoute, loginRoute, registerRoute } from './values/routes';
 import RequireAuth from './components/RequireAuth/RequireAuth';
+import CodeEditorPage from './pages/Authorized/CodeEditor/CodeEditorPage';
 
 const Navigation = (): JSX.Element => {
   return (
@@ -14,6 +15,7 @@ const Navigation = (): JSX.Element => {
         <Route path={registerRoute} element={<RegisterPage />} />
         <Route element={<RequireAuth />}>
           <Route path={homeRoute} element={<HomePage />} />
+          <Route path={codeEditorRoute} element={<CodeEditorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
